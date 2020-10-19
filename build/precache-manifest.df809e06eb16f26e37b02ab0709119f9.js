af90d0d3e95e792f5c892d80877a570c60765d07
Max Bertfield <max@bertfield.com>
2020-10-18 20:05:06 -0400

    build

---
+++ build/precache-manifest.df809e06eb16f26e37b02ab0709119f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a9d706165ca5868d98f3af5082326b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e973c3c859655d4c8bb",
+    "url": "/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "5acd985be162f84e239c",
+    "url": "/static/css/main.4bb43c60.chunk.css"
+  },
+  {
+    "revision": "7e973c3c859655d4c8bb",
+    "url": "/static/js/2.1b35b46f.chunk.js"
+  },
+  {
+    "revision": "8cade41a316a5ef0b54ba1e08a74d0e1",
+    "url": "/static/js/2.1b35b46f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5acd985be162f84e239c",
+    "url": "/static/js/main.fbb05d77.chunk.js"
+  },
+  {
+    "revision": "4040b55ff6b868e876aa",
+    "url": "/static/js/runtime-main.2c219ed3.js"
+  }
+]);
