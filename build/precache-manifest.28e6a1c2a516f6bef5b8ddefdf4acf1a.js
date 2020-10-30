44b24ea7e19abc969c772c255f5785d09084fc5c
Max Bertfield <max@bertfield.com>
2020-10-29 22:42:39 -0400

    add build

---
+++ build/precache-manifest.28e6a1c2a516f6bef5b8ddefdf4acf1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4bec4a0dc62d34c20f7936282637d3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08f6e115dce688095d98",
+    "url": "/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "740613e4c7204562b754",
+    "url": "/static/css/main.7203aa13.chunk.css"
+  },
+  {
+    "revision": "08f6e115dce688095d98",
+    "url": "/static/js/2.f35ffa18.chunk.js"
+  },
+  {
+    "revision": "8cade41a316a5ef0b54ba1e08a74d0e1",
+    "url": "/static/js/2.f35ffa18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "740613e4c7204562b754",
+    "url": "/static/js/main.2042f3a9.chunk.js"
+  },
+  {
+    "revision": "4040b55ff6b868e876aa",
+    "url": "/static/js/runtime-main.2c219ed3.js"
+  }
+]);
